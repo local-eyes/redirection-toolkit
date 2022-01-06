7593c5a0c736fece42dfbd18ba5d04742c4c3dcb
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2022-01-07 04:33:26 +0530

    ui overhaul

--- app/post/local/app.js
+++ app/post/local/app.js
@@ -1,7 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const origin = window.location.origin;
+document.addEventListener('DOMContentLoaded', async function() {
+    const pathname = window.location.pathname;
+    setTimeout(() => {
+        window.location.href = `https://localeyes.in${pathname}`;
+    }, 3000);
+});
+
+function redirect() {
     const pathname = window.location.pathname
-    const host = window.location.host;
-    const protocol = window.location.protocol;
-    console.log({ origin: origin, pathname: pathname, host: host, protocol: protocol });
-})
+    window.location.href = `https://localeyes.in${pathname}`;
+}
